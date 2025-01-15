b3ba6aff4b696c6dc4bc476eab34bdebf44e2abd
DavidFPR <136135434+DavidFPR@users.noreply.github.com>
2025-01-15 16:41:07 -0600

    Initial commit

---
+++ src/components/Cargando/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cargando = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <img src="img/loading.gif" style={{ width: '5vw', margin: '100px' }} />
+        </div>
+    )
+}
+
+export default Cargando
